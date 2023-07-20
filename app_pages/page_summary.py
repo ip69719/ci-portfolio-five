bd02f925f2011ab70ee4699a2f75c18f1b03cc9a
ip69719 <ipeters@mail.com>
2023-07-20 16:17:03 +0000

    format project summary page to improve readability and comply with PEP8

--- app_pages/page_summary.py
+++ app_pages/page_summary.py
@@ -7,27 +7,36 @@ def page_summary_body():
 
     st.info(
         f"**General Information**\n"
-        f"* Powdery mildew is a fungal disease that affects a wide range of plants."
-        f"* It is one of the easier plant diseases to identify, as its symptoms are quite distinctive.\n"
+        f"* Powdery mildew is a fungal disease that affects a wide range of "
+        f"plants.\n"
+        f"* It is one of the easier plant diseases to identify, as its "
+        f"symptoms are quite distinctive.\n"
         f"* Infected plants display white powdery spots on the leaves.\n"
-        f"* Visual criteria are used to verify if the tree leaf is healthy or has powdery mildew..\n"
+        f"* Visual criteria are used to verify if the tree leaf is healthy or "
+        f"has powdery mildew.\n\n"
 
-        
-        f"* The Client is facing a challenge where their cherry plantations have been presenting powdery mildew."
-        f"The cherry plantation crop is one of the finest products in the portfolio and "
-        f"the company is concerned about supplying the market with a product of compromised quality.\n\n"
+        f"* The Client is facing a challenge where their cherry plantations "
+        f"have been presenting powdery mildew. The cherry plantation crop is "
+        f"one of the finest products in the portfolio and the company "
+        f"concerned about supplying the market with a product of compromised "
+        f"quality.\n\n"
 
         f"**Project Dataset**\n"
-        f"* The available dataset contains over four thousand images taken from the Client's crop fields."
-        f"* The images show healthy cherry leaves and cherry leaves that have powdery mildew.")
+        f"* The available dataset contains over four thousand images taken "
+        f"from the Client's crop fields.\n"
+        f"* The images show healthy cherry leaves and cherry leaves that "
+        f"have powdery mildew.")
 
     st.write(
         f"* For additional information, please visit and **read** the "
-        f"[Project README file](https://github.com/ip69719/ci-portfolio-five/blob/main/README.md).")
+        f"[Project README file](https://github.com/ip69719/ci-portfolio-five/blob/main/README.md).")  # noqa:E501
 
     st.success(
+        f"**Business Requirements**\n\n"
         f"The project has 2 business requirements:\n"
-        f"* 1 - The client is interested in conducting a study to visually differentiate a healthy cherry leaf "
-        f"from one with powdery mildew."
-        f"* 2 - The client is interested in predicting if a cherry leaf is healthy or contains powdery mildew. "
+        f"* 1 - The client is interested in conducting a study to visually "
+        f"differentiate a healthy cherry leaf "
+        f"from one with powdery mildew.\n"
+        f"* 2 - The client is interested in predicting if a cherry leaf is "
+        f"healthy or contains powdery mildew. "
         )
