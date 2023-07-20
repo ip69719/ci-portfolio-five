31ae5561a037fe0a3b1d2ddeb6efea81938e6eec
ip69719 <ipeters@mail.com>
2023-07-20 18:31:10 +0000

    Create project hypothesis page for display on the dashboard

---
+++ app_pages/project_hypothesis.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+
+def project_hypothesis_body():
+    st.write("### Project Hypothesis and Validation")
+
+    st.success(
+        f"* We suspect cherry leaves infected with powdery mildew to have "
+        f"white powdery looking patches differentiating them from healthy "
+        f"green leaves."
+    )
