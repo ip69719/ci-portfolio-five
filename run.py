07bb83f42adfb59f8e2f7c3f03da8ece684df63c
ip69719 <ipeters@mail.com>
2023-07-20 18:41:01 +0000

    Create project hypothesis page for display on the dashboard

--- run.py
+++ run.py
@@ -2,11 +2,13 @@ import streamlit as st
 from app_pages.multipage import MultiPage
 
 from app_pages.page_summary import page_summary_body
+from app_pages.project_hypothesis import project_hypothesis_body
 
 # create an instance of MultiPage class
 app = MultiPage(app_name = "Cherry Leaf Powdery Mildew Detector")
 
 # add app pages here using .add_page() method
 app.add_page("Quick Project Summary", page_summary_body)
+app.add_page("Project Hypothesis", project_hypothesis_body)
 
 app.run()
