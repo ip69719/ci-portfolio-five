59d272ee5b2fc6a614ce50c33c8d720bcc8513fa
ip69719 <ipeters@mail.com>
2023-07-27 14:07:22 +0000

    Create function to display three checkboxes on leaf visualiser page

--- app_pages/page_cherry_leaf_visualiser.py
+++ app_pages/page_cherry_leaf_visualiser.py
@@ -3,3 +3,20 @@ import streamlit as st
 
 def page_cherry_leaf_visualiser_body():
     st.write("### Cherry Leaf Visualiser")
+    st.info(
+        f"The Client is interested in conducting a study to visually "
+        f"differentiate a healthy cherry leaf "
+        f"from one with powdery mildew.\n"
+
+    )
+
+    version = 'v1'
+
+    if st.checkbox("Difference between average and variability images"):
+        st.warning("Average and variability of images per label goes here")
+
+    if st.checkbox("Difference between average healthy leaf and average leaf with powdery mildew"):
+        st.warning("Difference between average healthy leaf and average leaf with powdery mildewgoes here")
+
+    if st.checkbox("Image montage"):
+        st.warning("Image montage goes here")
