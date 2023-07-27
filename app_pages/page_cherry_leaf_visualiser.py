22d7c12391becd2cacddf7b0957f660e3fed63f9
ip69719 <ipeters@mail.com>
2023-07-27 15:57:39 +0000

    Update leaf_visualiser_body function to display images on leaf visualiser page and copy image montage function

--- app_pages/page_cherry_leaf_visualiser.py
+++ app_pages/page_cherry_leaf_visualiser.py
@@ -1,6 +1,10 @@
 import streamlit as st
+import os
+import seaborn as sns
 import matplotlib.pyplot as plt
 from matplotlib.image import imread
+import itertools
+import random
 
 
 def page_cherry_leaf_visualiser_body():
@@ -28,4 +32,63 @@ def page_cherry_leaf_visualiser_body():
         st.write("---")
 
     if st.checkbox("Image montage"):
-        st.warning("Image montage goes here")
+        my_data_dir = 'inputs/mildew_dataset/cherry-leaves'
+        labels = os.listdir(my_data_dir+ '/validation')
+        label_to_display = st.selectbox(label="Select label", options=labels, index=0)
+        if st.button("Create Montage"):
+            image_montage(dir_path= my_data_dir + '/validation', label_to_display=label_to_display, nrows=3, ncols=3, figsize=(10,12))
+
+
+def image_montage(dir_path, label_to_display, nrows, ncols, figsize=(10,12)):
+    """
+    copy function from 02 - DataVisualization.ipynb notebook
+    called when Create Montage button is clicked
+
+    The function
+    * checks if the label exists in the directory
+    * checks if montage space is greater than the subset size
+    * creates a list of axes indices based on number of rows and number of columns
+    * creates a figure and display images
+    * loads and plots the images using a loop
+    """
+
+    sns.set_style("white")
+    labels = os.listdir(dir_path)
+    
+    # subset the class you are interested to display
+    if label_to_display in labels:
+        # checks if your montage space is greater than subset size
+        # how many images in that folder
+        images_list = os.listdir(dir_path+'/'+ label_to_display)
+        if nrows * ncols < len(images_list):
+            img_idx = random.sample(images_list, nrows * ncols)
+        else:
+            print(
+                f"Decrease nrows or ncols to create your montage. \n"
+                f"There are {len(images_list)} in your subset. "
+                f"You requested a montage with {nrows * ncols} spaces")
+            return
+    
+
+        # create list of axes indices based on nrows and ncols
+        list_rows= range(0,nrows)
+        list_cols= range(0,ncols)
+        plot_idx = list(itertools.product(list_rows,list_cols))
+
+
+        # create a Figure and display images
+        fig, axes = plt.subplots(nrows=nrows,ncols=ncols, figsize=figsize)
+        for x in range(0,nrows*ncols):
+            img = imread(dir_path + '/' + label_to_display + '/' + img_idx[x])
+            img_shape = img.shape
+            axes[plot_idx[x][0], plot_idx[x][1]].imshow(img)
+            axes[plot_idx[x][0], plot_idx[x][1]].set_title(f"Width {img_shape[1]}px x Height {img_shape[0]}px")
+            axes[plot_idx[x][0], plot_idx[x][1]].set_xticks([])
+            axes[plot_idx[x][0], plot_idx[x][1]].set_yticks([])
+        plt.tight_layout()
+        
+        st.pyplot(fig=fig)
+    
+    else:
+        print("The label you selected doesn't exist.")
+        print(f"The existing options are: {labels}")
