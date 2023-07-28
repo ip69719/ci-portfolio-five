3b57fa2997d13f53a745de6d8317a47f5f1c11f0
ip69719 <ipeters@mail.com>
2023-07-28 21:11:00 +0000

    Create ML performance page for the dashboard

--- app_pages/page_ml_performance.py
+++ app_pages/page_ml_performance.py
@@ -1,5 +1,29 @@
 import streamlit as st
+import matplotlib.pyplot as plt
+import pandas as pd
 
 
 def page_ml_performance_body():
-    st.write("### ML Performance Metrics")
+    
+    version = 'v1'
+
+    st.write("### Train, Validation and Test Set: Labels Frequencies")
+
+    labels_distribution = plt.imread(f"outputs/{version}/labels_distribution.png")
+    st.image(labels_distribution, caption='Labels Distribution on Train, Validation and Test Sets')
+    st.write("---")
+
+
+    st.write("### Model History")
+    col1, col2 = st.beta_columns(2)
+    with col1: 
+        model_acc = plt.imread(f"outputs/{version}/model_training_acc.png")
+        st.image(model_acc, caption='Model Training Accuracy')
+    with col2:
+        model_loss = plt.imread(f"outputs/{version}/model_training_losses.png")
+        st.image(model_loss, caption='Model Training Losses')
+    st.write("---")
+
+    st.write("### Generalised Performance on Test Set")
+    # save pickle file in output folder
+    st.dataframe(pd.DataFrame(load_test_evaluation(version), index=['Loss', 'Accuracy']))
