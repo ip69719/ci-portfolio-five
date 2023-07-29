fc69f4f41bc6da0ba218e71a2b8e1a426cf0f8c9
ip69719 <ipeters@mail.com>
2023-07-29 15:45:28 +0000

    Reformat page_ml_performance.py file to comply with PEP8

--- app_pages/page_ml_performance.py
+++ app_pages/page_ml_performance.py
@@ -5,19 +5,22 @@ from src.machine_learning.evaluate_clf import load_test_evaluation
 
 
 def page_ml_performance_body():
-    
+
     version = 'v1'
 
     st.write("### Train, Validation and Test Set: Labels Frequencies")
 
-    labels_distribution = plt.imread(f"outputs/{version}/labels_distribution.png")
-    st.image(labels_distribution, caption='Labels Distribution on Train, Validation and Test Sets')
+    labels_distribution = plt.imread(
+        f"outputs/{version}/labels_distribution.png")
+    st.image(
+        labels_distribution,
+        caption='Labels Distribution on Train, Validation and Test Sets'
+        )
     st.write("---")
-
 
     st.write("### Model History")
     col1, col2 = st.beta_columns(2)
-    with col1: 
+    with col1:
         model_acc = plt.imread(f"outputs/{version}/model_training_acc.png")
         st.image(model_acc, caption='Model Training Accuracy')
     with col2:
@@ -27,4 +30,5 @@ def page_ml_performance_body():
 
     st.write("### Generalised Performance on Test Set")
 
-    st.dataframe(pd.DataFrame(load_test_evaluation(version), index=['Loss', 'Accuracy']))
+    st.dataframe(pd.DataFrame(
+        load_test_evaluation(version), index=['Loss', 'Accuracy']))
