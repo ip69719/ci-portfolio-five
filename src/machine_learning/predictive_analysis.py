79a51cd763668ea618c9b7096b8fb0a8ced5fbfb
ip69719 <ipeters@mail.com>
2023-07-30 00:55:08 +0000

    Change label descriptions in load model and predict function for clearer results display to the User

--- src/machine_learning/predictive_analysis.py
+++ src/machine_learning/predictive_analysis.py
@@ -55,13 +55,13 @@ def load_model_and_predict(my_image, version):
 
     pred_proba = model.predict(my_image)[0, 0]
 
-    target_map = {v: k for k, v in {'healthy': 0, 'powdery_mildew': 1}.items()}
+    target_map = {v: k for k, v in {'is healthy': 0, 'has powdery mildew': 1}.items()}
     pred_class = target_map[pred_proba > 0.5]
     if pred_class == target_map[0]:
         pred_proba = 1 - pred_proba
 
     st.write(
-        f"The predictive analysis indicates the sample leaf has "
+        f"The predictive analysis indicates the sample leaf "
         f"**{pred_class.lower()}**.")
 
     return pred_proba, pred_class
