7d5519846888b53a243f31504e77612181dd0ab7
ip69719 <ipeters@mail.com>
2023-07-30 01:32:04 +0000

    Add section on hypothesis validation to project hypothesis page

--- app_pages/project_hypothesis.py
+++ app_pages/project_hypothesis.py
@@ -7,5 +7,10 @@ def project_hypothesis_body():
     st.success(
         f"* We suspect cherry leaves infected with powdery mildew to have "
         f"white powdery looking patches differentiating them from healthy "
-        f"green leaves."
+        f"green leaves. \n\n"
+        f"* An Image Montage shows that leaves containing powdery mildew "
+        f"typically have white patches on them. Healthy leaves are light "
+        f"to dark green in colour. Average Image, Variability Image and "
+        f"Difference between Averages studies did not reveal any clear "
+        f"pattern to differentiate one from another."
     )
